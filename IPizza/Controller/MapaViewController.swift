e1b8ca913383f0bf8d5600d97f71bffeed17866d
PUCPR <pucpr@ladmac0111.local>
2019-05-13 21:38:38 -0300

    Mapa adicionando várias anotações e mostrando onde você está.

--- IPizza/Controller/MapaViewController.swift
+++ IPizza/Controller/MapaViewController.swift
@@ -9,117 +9,104 @@
 import UIKit
 import MapKit
 
-class MapaViewController: UIViewController {
+class MapaViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapaView: MKMapView!
     let lm = CLLocationManager()
+    static let geocoder = CLGeocoder()
+    var editUsuario: Usuarios!
     
-    var endereco : String = "Raymundo Nina Rodrigues, 910 - 82920-010 - Cajuru - Curitiba - Paraná"
-    var nomeFantasia : String = "André Marafigo"
+    var x: Int!
+    var qtdEnderecos: Int!
     
-    var endereco2 : String = "Raymundo Nina Rodrigues, 900 - 82920-010 - Cajuru - Curitiba - Paraná"
-    var nomeFantasia2 : String = "Pizzaria"
-    
-    @IBOutlet weak var mapa: MKMapView!
-    
+    //    let localInicial = CLLocation(latitude: -29.3666, longitude: -52.2612)
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        tabBarItem = UITabBarItem(title: "Mapa", image: UIImage(named: "icons8-mapa-48"), tag: 1)
+        tabBarItem = UITabBarItem(title: "Mapa", image: UIImage(named: "icons8-mapa-48"), tag: 3)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Precisão da localização
+        lm.delegate = self
         lm.desiredAccuracy = kCLLocationAccuracyHundredMeters
-        //Solicita Permissão
         lm.requestWhenInUseAuthorization()
-        //Starta o GPS
-        //lm.startUpdatingLocation()
+        lm.startUpdatingLocation()
         
-        UIApplication.shared.beginIgnoringInteractionEvents()
-        
-        let activityIndicator = UIActivityIndicatorView(style: .gray)
-        activityIndicator.center = self.view.center
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.startAnimating()
-        
-        self.view.addSubview(activityIndicator)
-        
-        let searchRequest = MKLocalSearch.Request()
-        searchRequest.naturalLanguageQuery = endereco
-        
-        let activeSearch = MKLocalSearch(request: searchRequest)
-        
-        activeSearch.start { (response, error) in
-            
-            activityIndicator.stopAnimating()
-            UIApplication.shared.endIgnoringInteractionEvents()
-            
-            if response == nil {
-                print("ERRO")
-            }else {
-                //Busca dados
-                let latitude = response?.boundingRegion.center.latitude
-                let longitude = response?.boundingRegion.center.longitude
-                
-                //Cria anotação
-                let anotacao = MKPointAnnotation()
-                anotacao.title = self.nomeFantasia // self.endereco?.cliente?.nome
-                anotacao.subtitle = self.endereco
-                anotacao.coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
-                self.mapa.addAnnotation(anotacao)
-                
-                //Zoom
-                let coordenada: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude!, longitude!)
-                let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
-                let regiao = MKCoordinateRegion(center: coordenada, span: span)
-                self.mapa.setRegion(regiao, animated: true)
-            }
-        }
-        
-        
-        
-        UIApplication.shared.beginIgnoringInteractionEvents()
-        
-        let activityIndicator2 = UIActivityIndicatorView(style: .gray)
-        activityIndicator2.center = self.view.center
-        activityIndicator2.hidesWhenStopped = true
-        activityIndicator2.startAnimating()
-        
-        self.view.addSubview(activityIndicator2)
-        
-        let searchRequest2 = MKLocalSearch.Request()
-        searchRequest2.naturalLanguageQuery = endereco2
-        
-        let activeSearch2 = MKLocalSearch(request: searchRequest2)
-        
-        activeSearch2.start { (response, error) in
-            
-            activityIndicator2.stopAnimating()
-            UIApplication.shared.endIgnoringInteractionEvents()
-            
-            if response == nil {
-                print("ERRO")
-            }else {
-                //Busca dados
-                let latitude = response?.boundingRegion.center.latitude
-                let longitude = response?.boundingRegion.center.longitude
-                
-                //Cria anotação
-                let anotacao = MKPointAnnotation()
-                anotacao.title = self.nomeFantasia2 // self.endereco?.cliente?.nome
-                anotacao.subtitle = self.endereco2
-                anotacao.coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
-                self.mapa.addAnnotation(anotacao)
-                
-                //Zoom
-                let coordenada: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude!, longitude!)
-                let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
-                let regiao = MKCoordinateRegion(center: coordenada, span: span)
-                self.mapa.setRegion(regiao, animated: true)
-            }
-        }
-        
-    }
+        mapaView.mapType = .standard
 
+        let lat = -25.4456301
+        let long = -49.2126449
+        
+        //Usa localização atual
+//        let lat = Double((lm.location?.coordinate.latitude)!)
+//        let long = Double((lm.location?.coordinate.longitude)!)
+        
+        let center = CLLocationCoordinate2DMake(lat, long)
+        let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+        let regiao = MKCoordinateRegion(center: center, span: span)
+        self.mapaView.setRegion(regiao, animated: true)
+
+        let anotacao = MKPointAnnotation()
+        anotacao.coordinate = center
+        anotacao.title = "Você está Aqui!"
+
+        self.mapaView.addAnnotation(anotacao)
+        
+        let enderecos: [String] = [
+            "Raymundo Nina Rodrigues, 910 - 82920-010",
+            "Luiz França, 3083 - 82920-010",
+            "Raymundo Nina Rodrigues, 841 - 82920-010",
+            "Raymundo Nina Rodrigues, 750 - 82920-010"
+        ]
+        qtdEnderecos = enderecos.count
+        x = 0
+
+        addAnnotations(enderecos: enderecos)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let local = locations[locations.count-1]
+        
+        if local.horizontalAccuracy > 0.0 {
+            lm.stopUpdatingLocation()
+            print("\(local.coordinate.latitude), \(local.coordinate.longitude)")
+            MapaViewController.geocoder.reverseGeocodeLocation(local) { (placemarks, _) in
+                if let marca = placemarks?.first {
+                    print(marca)
+                }
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("\(error)")
+    }
+    
+    
+    func addAnnotations(enderecos: [String]) {
+        if x < qtdEnderecos {
+            MapaViewController.geocoder.geocodeAddressString(enderecos[self.x]) { (placemarks, error) in
+                guard
+                    let placemarks = placemarks,
+                    let location = placemarks.first?.location
+                    else {
+                        print("Erro location!!")
+                        return
+                }
+                
+                let anotacao = MKPointAnnotation()
+                anotacao.coordinate = location.coordinate
+                anotacao.title = "Aqui!"
+                
+                self.mapaView.addAnnotation(anotacao)
+                self.x += 1
+                self.addAnnotations(enderecos: enderecos)
+            }
+        }
+    }
 }
